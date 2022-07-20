b7a715d00843e895f87f08ba28df7cdfcfb8d643
TeodorDevios <81411970+TeodorDevios@users.noreply.github.com>
2022-07-21 00:01:00 +0300

    Update main_chat.py

--- moduls/main_chat.py
+++ moduls/main_chat.py
@@ -1,14 +1,25 @@
 from aiogram.types import Message
 from aiogram import types, Dispatcher
-from config import bot, dp
+from config import bot, dp, texts
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 from aiogram.dispatcher.filters.state import State, StatesGroup
 from aiogram.dispatcher import FSMContext
 
 
 async def start_message(message: Message):
-    await bot.send_message(message.chat.id, 'Hello world!')
+    await bot.send_message(message.chat.id, texts['start'], parse_mode='HTML')
+
+
+async def help_message(message: Message):
+    await bot.send_message(message.chat.id, f'Лечу на помощь: \n{texts["help"]}')
+
+
+async def report(message: Message):
+    await bot.send_message(message.chat.id, texts['report'][0])
 
 
 def init_chat(dp: Dispatcher):
     dp.register_message_handler(start_message, commands=['start'])
+    dp.register_message_handler(help_message, commands=['help'])
+    dp.register_message_handler(report, commands=['report'])
+
