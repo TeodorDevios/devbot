0a382907a31257d4b506b0d9c8e5ea0d5b091b6d
TeodorDevios <81411970+TeodorDevios@users.noreply.github.com>
2022-07-21 00:01:34 +0300

    Update config.py

--- config.py
+++ config.py
@@ -4,3 +4,10 @@ from aiogram.contrib.fsm_storage.memory import MemoryStorage
 storage = MemoryStorage()
 bot = Bot('5527733278:AAFp1Q4D6dE-OOzKgfsm-Y4afuxM8afs_zI')
 dp = Dispatcher(bot, storage=storage)
+texts = {
+    'start': '<i>Hello world!</i> \nЯ - Дев, лучший помощник для вашего чата! \nНапиши команду /help, чтобы узнать больше',
+    'help': 'https://telegra.ph/CHat-bot-Dev-Kak-polzovatsya-07-20',
+    'report': ['Нашли баг? Есть предложения? Напиши тему репорта, например: "Невкусный компот"',
+               'Такс, а теперь опиши суть'
+               ]
+}
