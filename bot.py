d0ee754fddb5173bff45b35fa6a245174e44fc2e
TeodorDevios <81411970+TeodorDevios@users.noreply.github.com>
2022-07-20 22:11:41 +0300

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,10 @@
+from aiogram import executor
+from config import dp
+
+
+async def on_startup(_):
+    print('Я работаю, хозяинка')
+
+
+init_chat(dp)
+executor.start_polling(dp, skip_updates=True)
