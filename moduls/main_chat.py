db0aea6ee87709885e3e4e0e688d6b8e0e3da4ce
TeodorDevios <81411970+TeodorDevios@users.noreply.github.com>
2022-07-22 22:26:27 +0300

    Update main_chat.py

--- moduls/main_chat.py
+++ moduls/main_chat.py
@@ -1,7 +1,6 @@
 from aiogram.types import Message
 from aiogram import types, Dispatcher
-from config import bot, dp, texts
-from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+from config import bot, dp, texts, check_language
 from aiogram.dispatcher.filters.state import State, StatesGroup
 from aiogram.dispatcher import FSMContext
 
@@ -10,7 +9,9 @@ class Report(StatesGroup):
     report_headline = State()
     report_text = State()
 
+
 class Code(StatesGroup):
+    code_language = State()
     code_info = State()
     exs = ''
 
@@ -41,32 +42,37 @@ async def report_headline(message: Message, state: FSMContext):
 async def report_text(message: Message, state: FSMContext):
     async with state.proxy() as data:
         data['report_text'] = message.text
+    await state.finish()
     await bot.send_message(message.chat.id, 'Репорт отправлен. Ожидай ответа =)')
 
 
 async def generate_code(message: Message):
-    await message.reply("""Выберите соответствующий язык:
-    /py - Python
-    /html - HTML
-    /css - CSS
-    /js - JavaScript""")
+    await message.reply("""Выбери язык:
+    py - Python
+    html - HTML
+    css - CSS
+    js - JavaScript""")
+    await Code.code_language.set()
 
 
-async def file_with_code(message: Message):
-    langs = ['py', 'html', 'css', 'js']
-    if message.text[1:] in langs:
-        Code.exs = message.text[1:]
-        await bot.send_message(message.chat.id, "Присылай код")
-        await Code.code_info.set()
+async def file_with_code(message: Message, state: FSMContext):
+    async with state.proxy() as data:
+        data['code_language'] = message.text.lower()
+    is_code = check_language(data['code_language'])
+    if is_code:
+        await message.reply('А теперь кидай код')
+        await Code.next()
+    else:
+        await message.reply('Ты не умеешь читать, солнышко?) Повтори попытку еще раз')
 
 
-async def code_info(message: Message, state = FSMContext):
-    mes = message.text
+async def code_info(message: Message, state: FSMContext):
+    code = message.text
     file = 'lol' + '.' + Code.exs
     files = open(file, "w")
-    files.write(mes)
+    files.write(code)
     files = open(file, "rb")
-    await bot.send_document(chat_id = message.chat.id, document = files)
+    await bot.send_document(chat_id=message.chat.id, document=files)
     files.close()
     await state.finish()
 
@@ -78,6 +84,5 @@ def init_chat(dp: Dispatcher):
     dp.register_message_handler(report_headline, state=Report.report_headline)
     dp.register_message_handler(report_text, state=Report.report_text)
     dp.register_message_handler(generate_code, commands=['generate_code'])
-    dp.register_message_handler(file_with_code, commands=['py', 'html', 'css', 'js'])
+    dp.register_message_handler(file_with_code, state=Code.code_language)
     dp.register_message_handler(code_info, state=Code.code_info)
-
