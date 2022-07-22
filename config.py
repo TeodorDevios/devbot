b52558a5a10248bc31e9f6f3c293b22d264df4eb
TeodorDevios <81411970+TeodorDevios@users.noreply.github.com>
2022-07-22 22:46:43 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,12 +1,18 @@
 from aiogram import Dispatcher, Bot
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
-# переменные для хранения токена, бота и его памяти. еще диспетчера
 storage = MemoryStorage()
 bot = Bot('5527733278:AAFp1Q4D6dE-OOzKgfsm-Y4afuxM8afs_zI')
 dp = Dispatcher(bot, storage=storage)
 texts = {
-    'start': '<i>Hello world!</i> \nЯ - Дев, лучший помощник для вашего чата! \nНапиши команду /help, чтобы узнать больше',
-    'help': 'https://telegra.ph/CHat-bot-Dev-Kak-polzovatsya-07-20',
+    'start': '''<i>Hello world!</i> \n
+Я <b>Дев</b> — первый бот-программист в телеграмме..ну или почти
+Я смогу помочь тебе:
+- развлечь участников твоей группы
+- записать простой код в файл
+- помочь избавиться от спамеров
+и многое-многое другое...как же все это во мне помещается то?
+Тебе нужно всего лишь написать команду /help и добавить меня в чат =3''',
+    'help': 'https://telegra.ph/CHat-bot-Dev-Komandy-07-22',
     'report': [
                'Нашли баг? Есть предложения? Напиши тему репорта, например: "Невкусный компот"',
                'Такс, а теперь опиши суть'
@@ -17,5 +23,5 @@ langs = ['py', 'html', 'css', 'js']
 
 def check_language(language):
     if language in langs:
-        return True
-    return False
+        return [language, True]
+    return ['so', False]
