c9e757555fc74c643f949e493f3681a558543c7d
TeodorDevios <81411970+TeodorDevios@users.noreply.github.com>
2022-07-24 22:37:52 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,11 +1,13 @@
 from aiogram import executor
 from config import dp
-from moduls.main_chat import init_chat
+from moduls.main_chat import init_main_chat
+from moduls.main_admin import init_main_admin
 
 
 async def on_startup(_):
     print('Я работаю, хозяинка')
 
 
-init_chat(dp)
+init_main_chat(dp)
+init_main_admin(dp)
 executor.start_polling(dp, skip_updates=True)
